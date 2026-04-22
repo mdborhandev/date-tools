f3a53443a9df09b977bc999b94de35541d4a7509
mdborhandev <mdborhan.dev@gmail.com>
2026-04-22 17:22:09 +0600

    sss

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AgeCalculator from './AgeCalculator';
-import 'flatpickr/dist/flatpickr.min.css';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AgeCalculator />);
